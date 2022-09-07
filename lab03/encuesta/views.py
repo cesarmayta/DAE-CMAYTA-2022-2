5a35ea92e14c925f35461d08345f1d2a49f10ba6
cesar mayta <cesarmayta@gmail.com>
2022-09-07 17:44:03 -0500

    lab03 prueba

--- lab03/encuesta/views.py
+++ lab03/encuesta/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
+from .models import Pregunta,Opcion
+
 # Create your views here.
+def index(request):
+    lista_preguntas = Pregunta.objects.all()
+    #select * from encuesta_pregunta
+    print(lista_preguntas)
+
+    context = {
+        'preguntas':lista_preguntas
+    }
+
+    return render(request,'encuesta/index.html',context)
